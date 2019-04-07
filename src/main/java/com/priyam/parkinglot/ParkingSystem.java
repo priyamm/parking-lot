1872f4a9e6e3c4bebd20c7650592726ab06f1478
Priyam Gupta <prigupta8@publicisgroupe.net>
2019-04-07 13:44:40 +0530

    added-parking-system-input-mechanism

--- src/main/java/com/priyam/parkinglot/ParkingSystem.java
+++ src/main/java/com/priyam/parkinglot/ParkingSystem.java
@@ -1,5 +1,69 @@
 package com.priyam.parkinglot;
 
+import com.priyam.parkinglot.model.ParkingLot;
+
+import java.util.Scanner;
+
 public class ParkingSystem {
+
+    public static void main(String...strings) {
+
+        Scanner scanner = new Scanner(System.in);
+        ParkingLot parkinglot = null;
+        while(true != false) {
+            String command = scanner.next();
+            switch(command) {
+                case "exit":
+                    System.exit(0);
+                    break;
+                case "create_parking_lot":
+                    parkinglot = new ParkingLot(scanner.nextInt());
+                    System.out.println(String.format("Created a parking lot with %s slots", parkinglot.getSize()));
+                    break;
+                case "park":
+                    if(parkinglot == null)
+                        System.out.println("proivde the size of the parkin lot first");
+                    else
+                        System.out.println(parkinglot.parkVehicle(scanner.next(), scanner.next()));;
+                    break;
+                case "leave":
+                    if(parkinglot == null)
+                        System.out.println("proivde the size of the parkin lot first");
+                    else
+                        System.out.println(parkinglot.leaveVehice(scanner.nextInt()));
+                    break;
+                case "status":
+                    if(parkinglot == null)
+                        System.out.println("proivde the size of the parkin lot first");
+                    else
+                        System.out.println(parkinglot.getStatus());
+                    break;
+                case "registration_numbers_for_cars_with_colour":
+                    if(parkinglot == null)
+                        System.out.println("proivde the size of the parkin lot first");
+                    else
+                        System.out.println(parkinglot.getRegistrationNumbersByCarColor(scanner.next()));
+                    break;
+                case "slot_numbers_for_cars_with_colour":
+                    if(parkinglot == null)
+                        System.out.println("proivde the size of the parkin lot first");
+                    else
+                        System.out.println(parkinglot.getSlotNumbersByCarColor(scanner.next()));
+                    break;
+                case "slot_number_for_registration_number":
+                    if(parkinglot == null)
+                        System.out.println("proivde the size of the parkin lot first");
+                    else
+                        System.out.println(parkinglot.getSlotNumberByCarRegistrationNumber(scanner.next()));
+                    break;
+
+                default:
+                    System.out.println("Invalid command");
+                    break;
+            }
+        }
+
+
+    }
 
 }
--- src/main/java/com/priyam/parkinglot/model/ParkingLot.java
+++ src/main/java/com/priyam/parkinglot/model/ParkingLot.java
@@ -7,18 +7,34 @@ public class ParkingLot {
     private Integer size;
     private ParkingSlot[] slots;
 
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public ParkingSlot[] getSlots() {
+        return slots;
+    }
+
+    public void setSlots(ParkingSlot[] slots) {
+        this.slots = slots;
+    }
+
     public ParkingLot(Integer size) {
         this.size = size;
         this.slots = new ParkingSlot[this.size];
 
-        for(int i = 0 ; i < size ; i++) {
+        for(int i = 1 ; i <= size ; i++) {
             slots[i] = new ParkingSlot(i);
         }
 
     }
 
     public String parkVehicle(String registationNumber, String color) {
-        for(int i = 0 ; i < size ; i++) {
+        for(int i = 1 ; i <= size ; i++) {
             if(slots[i].getFree()) {
                 slots[i].assignVehice(new Car(registationNumber, color));
                 return String.format("Allocated slot number: %s", i);
@@ -37,7 +53,7 @@ public class ParkingLot {
 
     public String getStatus() {
         String status = "Slot No. Registration No Colour\n";
-        for(int i = 0; i < size ; i++) {
+        for(int i = 1; i <= size ; i++) {
             if(!slots[i].getFree()) {
                 status += i + " " + slots[i].getCar().getRegistrationNumber() + " " + slots[i].getCar().getColor() + "\n";
             }
@@ -47,26 +63,26 @@ public class ParkingLot {
 
     public String getRegistrationNumbersByCarColor(String color) {
         String registrationNumbers = "";
-        for(int i = 0 ; i < size ; i++) {
+        for(int i = 1 ; i <= size ; i++) {
             if(!slots[i].getFree()) {
-                registrationNumbers += slots[i].getCar().getRegistrationNumber() + ",";
+                registrationNumbers += slots[i].getCar().getRegistrationNumber() + ", ";
             }
         }
-        return registrationNumbers.charAt(registrationNumbers.length() - 1) == ',' ? registrationNumbers.substring(0, registrationNumbers.length() - 1) : registrationNumbers;
+        return registrationNumbers.charAt(registrationNumbers.length() - 1) == ' ' ? registrationNumbers.substring(0, registrationNumbers.length() - 2) : registrationNumbers;
     }
 
     public String getSlotNumbersByCarColor(String color) {
         String slotNumbers = "";
-        for(int i = 0 ; i < size ; i++) {
+        for(int i = 1 ; i <= size ; i++) {
             if(!slots[i].getFree()) {
-                slotNumbers += i;
+                slotNumbers += i + ", ";
             }
         }
-        return slotNumbers.charAt(slotNumbers.length() - 1) == ',' ? slotNumbers.substring(0, slotNumbers.length() - 1) : slotNumbers;
+        return slotNumbers.charAt(slotNumbers.length() - 1) == ' ' ? slotNumbers.substring(0, slotNumbers.length() - 2) : slotNumbers;
     }
 
     public String getSlotNumberByCarRegistrationNumber(String registrationNumber) {
-        for(int i = 0 ; i < size ; i++) {
+        for(int i = 1 ; i <= size ; i++) {
             if(!slots[i].getFree() && slots[i].getCar().getRegistrationNumber().equals(registrationNumber))
                 return String.valueOf(i);
         }
